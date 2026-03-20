84814e54c59e3282d2ba6b3a530dbf6c0cb2a6d0
Jamie <jamie.giarratana@gmail.com>
2026-03-20 16:19:11 -0400

    hypr stuff, screenshotting etc

---
+++ .config/hypr/scripts/dot-screenshot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Take a screenshot in a number of different ways. Any of the case statement
+# options can be passed in as a value.
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+MODE="${1:-region}"
+
+case "${MODE}" in
+region)
+  hyprshot -m region -r --
+  ;;
+window)
+  hyprshot -m window -r --
+  ;;
+monitor-focused)
+  grim -o "$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')" -
+  ;;
+monitor-all)
+  grim -
+  ;;
+*)
+  echo "'${MODE}' is not a supported, aborting!" >&2
+  exit 1
+  ;;
+esac | satty --filename - --output-filename "$HOME/Pictures/Screenshots/screenshot-%+.png"
