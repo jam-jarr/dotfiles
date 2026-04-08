731fa357c29dcbfbc8004890b382df6b70662c73
Jamie <jamie.giarratana@gmail.com>
2026-04-08 13:18:55 -0400

    Automatic floatrule script

---
+++ .config/hypr/scripts/float-rule.sh
@@ -0,0 +1,14 @@
+# This script automatically sets a floating rule to the active window in hyprland
+set -eux
+
+class=$(hyprctl activewindow -j | jq -r '.class')
+
+title=$(hyprctl activewindow -j | jq -r '.title')
+
+# Append
+
+rule="windowrule = float = on, match:class = $class, match:title = $title"
+
+echo "$rule" >>~/.config/hypr/hyprland-floatlist.conf
+
+notify-send "Floating rule set for $title - $class"
