2954dfb8812c007504fb768ae47b8ca7cb2b184f
Jamie <jamie.giarratana@gmail.com>
2026-04-30 19:53:11 -0400

    hyprscripts: improve focus/efficient

--- .config/hypr/scripts/hyprefficient.sh
+++ .config/hypr/scripts/hyprefficient.sh
@@ -2,14 +2,15 @@
 set -eux
 FLAG_FILE='/tmp/hyprefficient'
 RULE_FILE="$HOME/.config/hypr/hyproverrules.conf"
-ICON_FILE="$HOME/.config/hypr/icons/hypr.ico"
+ICON_FILE_ON="$HOME/.config/hypr/icons/hypr.ico"
+ICON_FILE_OFF="$HOME/.config/hypr/icons/hypr_desaturated.ico"
 if [ ! -f "$FLAG_FILE" ]; then
   touch "$FLAG_FILE"
   echo "windowrule = match:class .*, opacity 1" >>"$RULE_FILE"
-  notify-send -t 700 -u low --transient --icon="$ICON_FILE" "Hyprefficient [ON]"
+  notify-send -t 700 -u low --transient --icon="$ICON_FILE_ON" "Hyprefficient [ON]"
 else
   # Sleep is currently enabled, so disable it
   rm -f "$FLAG_FILE"
   sed -i '/windowrule = match:class .*, opacity 1/d' "$RULE_FILE"
-  notify-send -t 700 -u low --transient --icon="$ICON_FILE" "Hyprefficient [OFF]"
+  notify-send -t 700 -u low --transient --icon="$ICON_FILE_OFF" "Hyprefficient [OFF]"
 fi
--- .config/hypr/scripts/hyprfocus.sh
+++ .config/hypr/scripts/hyprfocus.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+set -eux
+ICON_FILE_ON="$HOME/.config/hypr/icons/hypr.ico"
+ICON_FILE_OFF="$HOME/.config/hypr/icons/hypr_desaturated.ico"
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ]; then
   hyprctl --batch "\
         keyword animations:enabled 0;"
-  notify-send -t 700 -u low --transient --icon="$ICON_FILE" "Hyprfocus [ON]"
+  notify-send -t 700 -u low --transient --icon="$ICON_FILE_ON" "Hyprfocus [ON]"
   exit
 else
-  notify-send -t 700 -u low --transient --icon="$ICON_FILE" "Hyprfocus [OFF]"
+  notify-send -t 700 -u low --transient --icon="$ICON_FILE_OFF" "Hyprfocus [OFF]"
   hyprctl reload
   exit 0
 fi
