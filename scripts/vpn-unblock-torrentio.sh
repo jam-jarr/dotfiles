6d93b481ae5829cfd982de474029eba389f158fe
Jamie <jamie.giarratana@gmail.com>
2026-06-13 18:13:13 -0400

    scritps: Run stremio unblocking script in background

--- scripts/vpn-unblock-torrentio.sh
+++ scripts/vpn-unblock-torrentio.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-nordvpn connect >/dev/null
-while curl -s torrentio.strem.fun | rg -q block; do
+# Run the app before connecting to the VPN, to speed things up
+(
   nordvpn connect >/dev/null
-done
-echo "Time to steal 😻"
+  while curl -s torrentio.strem.fun | rg -q block; do
+    nordvpn connect >/dev/null
+  done
+) &
+
+/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=/app/opt/stremio/stremio --file-forwarding com.stremio.Stremio @@u %U @@
